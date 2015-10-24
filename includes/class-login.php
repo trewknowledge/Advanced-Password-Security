af2e9452387225256d1767481336c64376aa0aef
Fernando Claussen <fernandoclaussen@gmail.com>
2015-10-24 03:18:10 -0400

    Adds the wp_login action callback method that checks if the user password is expired during login and if it is, it destroys all sessions for that user and redirect him to the reset password form

--- includes/class-login.php
+++ includes/class-login.php
@@ -17,4 +17,21 @@ class Login {
 		add_filter( 'login_message', array( $this, 'lost_password_message' ) );
 	}
 
+	public function login( $user_login, $user ) {
+		if ( !APS::is_password_expired( $user ) ) {
+			return;
+		}
+
+		wp_destroy_all_sessions();
+
+		$redirect = add_query_arg(
+			array(
+				'action'        => 'lostpassword',
+			),
+			wp_login_url()
+		);
+		wp_safe_redirect( $redirect, 302 );
+		exit;
+	}
+
 }
