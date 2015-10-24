ce36a941e6ed885637608c096c839a976769cd1f
Fernando Claussen <fernandoclaussen@gmail.com>
2015-10-24 03:01:50 -0400

    Adding actions to the __construct function

--- includes/class-login.php
+++ includes/class-login.php
@@ -11,5 +11,10 @@ use Advanced_Password_Security as APS;
 class Login {
 
 	
+	public function __construct(){
+		add_action( 'wp_login', array( $this, 'login' ), 10, 2 );
+		add_action( 'validate_password_reset', array( $this, 'validate_password' ), 10, 2 );
+		add_filter( 'login_message', array( $this, 'lost_password_message' ) );
+	}
 
 }
