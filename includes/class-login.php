27016bbcb39c9cdc9a0d53730d35ec9e58f70ff6
Fernando Claussen <fernandoclaussen@gmail.com>
2015-10-24 03:30:43 -0400

    Validates the new password. If it is not the current password and if it is not any of the old passwords, add the new password to the list and update the user's reset date

--- includes/class-login.php
+++ includes/class-login.php
@@ -34,4 +34,37 @@ class Login {
 		exit;
 	}
 
+	public function validate_password( $errors, $user ) {
+		$new_pass = isset( $_POST['pass1'] ) && $_POST['pass1'] ? $_POST['pass1'] : '';
+		$new_pass2 = isset( $_POST['pass2'] ) && $_POST['pass2'] ? $_POST['pass2'] : '';
+
+		if ( !empty($new_pass) && $new_pass === $new_pass2 ) {
+			$same_pass = wp_check_password( $new_pass, $user->data->user_pass, $user->ID );
+			if ( $same_pass ) {
+				$errors->add( 'password_already_used', __( 'You cannot reuse your old password.', APS_TEXTDOMAIN ) );
+				return;
+			}
+
+			$used_passwords = APS::get_old_passwords( $user );
+
+			foreach ($used_passwords as $old_pass) {
+				if ( wp_check_password( $new_pass, $old_pass['pass'], $user->ID ) ) {
+					$errors->add( 'password_already_used', __( 'You cannot reuse a password that you used in the past.', APS_TEXTDOMAIN ) );
+					$errors->add( 'password_already_used', sprintf( __( 'You used this password back in %s.', APS_TEXTDOMAIN ), $old_pass['created'] ) );
+					return;
+				}
+			}
+
+			global $wpdb;
+			$hash = wp_hash_password( $new_pass );
+			$created = date('F jS, Y');
+			array_push( $used_passwords, array( 'pass' => $hash, 'created' => $created ) );
+			$used_passwords = maybe_serialize( $used_passwords );
+
+		    $wpdb->update( $wpdb->users, array( 'old_user_pass' => $used_passwords ), array( 'ID' => $user->ID ) );
+		 
+			update_user_meta( $user->ID, APS::META_KEY, gmdate( "U" ) );			
+		}
+	}
+
 }
