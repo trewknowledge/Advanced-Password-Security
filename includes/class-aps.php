b94f63cd9098ca19973abe0f5ef110312722ec9f
Fernando Claussen <fernandoclaussen@gmail.com>
2017-12-27 11:41:04 -0500

    You can no longer go to plugins page to disable the plugin. Deal with it

--- includes/class-aps.php
+++ includes/class-aps.php
@@ -165,7 +165,7 @@ class APS {
 	function redirect() {
 		if ( is_admin() ) {
 			$screen = get_current_screen();
-			if ( 'profile' === $screen->base || 'plugins' === $screen->base ) {
+			if ( 'profile' === $screen->base ) {
 				return;
 			}
 		}
