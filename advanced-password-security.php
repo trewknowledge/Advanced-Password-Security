703bc57bdef5d0569a1951bd85279354102e408f
Fernando Claussen <fernandoclaussen@gmail.com>
2017-12-29 12:57:29 -0500

    Update version and text domain

--- advanced-password-security.php
+++ advanced-password-security.php
@@ -4,11 +4,12 @@
  * Description:   Force your admins and/or select roles to update their password to a new one after a set amount of time.
  * Author:        Trew Knowledge
  * Author URI:    http://trewknowledge.com
- * Version:       1.0.0
+ * Version:       1.0.2
  * Plugin URI:    http://github.com/trewknowledge/advanced-password-security
  * License:       GPL-3.0+
  * License URI:   http://www.gnu.org/licenses/gpl-3.0.txt
  * Domain Path:   /i18n
+ * Text Domain: 	aps
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
